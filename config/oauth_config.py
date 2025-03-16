b335929510f2f830ff56a5f36e4049186b808590
Wandeson1994 <152650754+Wandeson1994@users.noreply.github.com>
2025-03-16 16:34:09 -0300

    Update oauth_config.py

--- config/oauth_config.py
+++ config/oauth_config.py
@@ -3,14 +3,14 @@ Configurações para autenticação OAuth.
 """
 
 # Configurações do Google OAuth
-GOOGLE_CLIENT_ID = "client_id"  # Substitua pelo seu Client ID do Google
-GOOGLE_CLIENT_SECRET = "client_secret"  # Substitua pelo seu Client Secret do Google
-GOOGLE_REDIRECT_URI = "redirect_uri"  # URL de redirecionamento após autenticação
+GOOGLE_CLIENT_ID = "759973083046-ldmssmmegiarirn1vomf2qvdk6ou4rb6.apps.googleusercontent.com"  # Substitua pelo seu Client ID do Google
+GOOGLE_CLIENT_SECRET = "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"  # Substitua pelo seu Client Secret do Google
+GOOGLE_REDIRECT_URI = "https://dashboard-wandersolutions.streamlit.app"  # URL de redirecionamento após autenticação
 
 # Configurações do Facebook OAuth
-FACEBOOK_CLIENT_ID = "client_id-1"  # Substitua pelo seu App ID do Facebook
-FACEBOOK_CLIENT_SECRET = "client_secret-1"  # Substitua pelo seu App Secret do Facebook
-FACEBOOK_REDIRECT_URI = "redirect_uri-1"  # URL de redirecionamento após autenticação
+FACEBOOK_CLIENT_ID = "1672423616965359"  # Substitua pelo seu App ID do Facebook
+FACEBOOK_CLIENT_SECRET = "f7b715501a2bab01f5ef3d19812c7047"  # Substitua pelo seu App Secret do Facebook
+FACEBOOK_REDIRECT_URI = "https://dashboard-wandersolutions.streamlit.app"  # URL de redirecionamento após autenticação
 
 # Escopos das permissões solicitadas
 GOOGLE_SCOPE = [
